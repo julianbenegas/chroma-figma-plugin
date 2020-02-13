d0273a2c238cddb956e85d5a516074f406c29628
julianbenegas <julianbenegas99@gmail.com>
2020-02-13 20:36:19 -0300

    almost done?

--- src/code.ts
+++ src/code.ts
@@ -1,4 +1,4 @@
-figma.showUI(__html__)
+figma.showUI(__html__, { height: 400, width: 320 })
 
 type rgb = number[]
 
@@ -6,21 +6,30 @@ interface Args {
   type: 'create-scale' | 'cancel'
   colors: rgb[]
   scale: chroma.Scale
+  localStyles: string
+  name: string
 }
 
-figma.ui.onmessage = ({ type, colors }: Args) => {
+figma.ui.onmessage = ({ type, colors, localStyles, name }: Args) => {
   if (type === 'create-scale') {
     const nodes = []
     for (let i = 0; i < colors.length; i++) {
       const [r, g, b] = colors[i]
       const rect = figma.createRectangle()
-      rect.x = i * 150
+      rect.x = i * 82
+      rect.resize(70, 70)
+      rect.cornerRadius = 8
       rect.fills = [{ type: 'SOLID', color: { r, g, b } }]
       figma.currentPage.appendChild(rect)
       nodes.push(rect)
+      if (localStyles === 'css' || localStyles === 'regular') {
+        const style = figma.createPaintStyle()
+        style.paints = [{ type: 'SOLID', color: { r, g, b } }]
+        if (localStyles === 'css') style.name = `--${name}-${i + 1}`
+        else style.name = `${name}-${i + 1}`
+      }
     }
     figma.currentPage.selection = nodes
     figma.viewport.scrollAndZoomIntoView(nodes)
-  }
-  figma.closePlugin()
+  } else if (type === 'cancel') figma.closePlugin()
 }
--- src/ui.tsx
+++ src/ui.tsx
@@ -3,24 +3,78 @@ import * as ReactDOM from 'react-dom'
 import './ui.css'
 import * as chroma from 'chroma-js'
 
-declare function require(path: string): any
+interface Data {
+  colors: string
+  count: string
+  localStyles: string
+  name: string | undefined
+  errors: {
+    colors?: string
+    count?: string
+    localStyles?: string
+    name?: string | undefined
+  }
+}
 
 const App = () => {
-  const [data, setData] = React.useState({ colors: '', count: '5' })
+  const [data, setData] = React.useState<Data>({
+    colors: '',
+    count: '5',
+    localStyles: 'no',
+    name: undefined,
+    errors: {}
+  })
+  const [withLocalStyles, setWithLocalStyles] = React.useState(false)
+  const [generalError, setGeneralError] = React.useState<string>('')
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setData({ ...data, [event.currentTarget.name]: event.currentTarget.value })
   }
 
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.currentTarget
+    if (value === 'css' || value === 'regular') setWithLocalStyles(true)
+    else setWithLocalStyles(false)
+    setData({ ...data, [event.currentTarget.name]: event.currentTarget.value })
+  }
+
   const onCreate = () => {
-    const scale = chroma
-      .scale(data.colors.split(',').map(c => c.trim()))
-      .colors(parseInt(data.count))
-    let colors = []
-    for (let i = 0; i < parseInt(data.count); i++) {
-      colors.push(chroma(scale[i]).gl())
+    // Validate
+    let errorsObj: any = {}
+    if (!data.colors.length) errorsObj.colors = "Can't be empty"
+    if (!data.count.length) errorsObj.count = "Can't be empty"
+    else if (isNaN(parseInt(data.count))) errorsObj.count = 'Not a number!'
+    if (data.localStyles !== 'no' && !data.name.length) {
+      errorsObj.name = "Can't be empty"
     }
-    parent.postMessage({ pluginMessage: { type: 'create-scale', colors } }, '*')
+    if (Object.keys(errorsObj).length) {
+      return setData({ ...data, errors: errorsObj })
+    }
+    setData({ ...data, errors: {} })
+    // Everything is nice and clean here
+    try {
+      const scale = chroma
+        .scale(data.colors.split(',').map(c => c.trim()))
+        .colors(parseInt(data.count))
+      let colors = []
+      for (let i = 0; i < parseInt(data.count); i++) {
+        colors.push(chroma(scale[i]).gl())
+      }
+      setGeneralError('')
+      parent.postMessage(
+        {
+          pluginMessage: {
+            type: 'create-scale',
+            colors,
+            localStyles: data.localStyles,
+            name: data.name
+          }
+        },
+        '*'
+      )
+    } catch (error) {
+      setGeneralError(error.message)
+    }
   }
 
   const onCancel = () => {
@@ -29,20 +83,65 @@ const App = () => {
 
   return (
     <div>
-      <img src={require('./logo.svg')} />
-      <h2>Rectangle Creator</h2>
-      <p>
-        Comma separated colors:{' '}
+      <div className="logo" />
+      <h2>Chroma Scale Creator</h2>
+      {generalError && <p className="general-error">{generalError}</p>}
+      <div className="input-container">
+        <label htmlFor="colors">Colors (separated by commas)</label>
         <input
           name="colors"
+          id="colors"
           type="text"
           value={data.colors}
           onChange={handleChange}
+          maxLength={1000}
         />
-      </p>
-      <p>
-        Count: <input name="count" value={data.count} onChange={handleChange} />
-      </p>
+        {data.errors.colors && (
+          <div className="error">{data.errors.colors}</div>
+        )}
+      </div>
+      <div className="input-container">
+        <label htmlFor="count">Number of colors</label>
+        <input
+          name="count"
+          id="count"
+          type="number"
+          value={data.count}
+          onChange={handleChange}
+        />
+        {data.errors.count && <div className="error">{data.errors.count}</div>}
+      </div>
+      <div className="input-container">
+        <label htmlFor="count">Save to local styles</label>
+        <select
+          name="localStyles"
+          id="localStyles"
+          value={data.localStyles}
+          onChange={handleSelectChange}
+        >
+          <option value="no" defaultChecked>
+            No
+          </option>
+          <option value="css">Yes, as CSS Props (--grey-1)</option>
+          <option value="regular">Yes please, regular (grey-1)</option>
+        </select>
+        {data.errors.localStyles && (
+          <div className="error">{data.errors.localStyles}</div>
+        )}
+      </div>
+      <div className="input-container">
+        <label htmlFor="colors">Name (for local styles)</label>
+        <input
+          name="name"
+          id="name"
+          type="text"
+          value={data.name}
+          onChange={handleChange}
+          maxLength={100}
+          disabled={!withLocalStyles}
+        />
+        {data.errors.name && <div className="error">{data.errors.name}</div>}
+      </div>
       <button id="create" onClick={onCreate}>
         Create
       </button>
